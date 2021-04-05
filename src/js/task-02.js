fd8f85b4da50b8e2d95ffff19a437bfb5e40043d
Serj-New <njersdesign@gmail.com>
2021-04-05 09:48:58 +0300

    добавляет решение задачи 03

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -11,6 +11,16 @@ const ulEl = document.querySelector('#ingredients');
 
 // Вариант 1
 
+const listEl = ingredients.map(item => {
+  const liEl = document.createElement('li');
+  liEl.textContent = item;
+  return liEl;
+})
+
+ulEl.append(...listEl);
+
+// Вариант 2
+
 // ingredients.forEach(item => {
 //   const liEl = document.createElement('li');
 //   liEl.textContent = item;
@@ -18,14 +28,14 @@ const ulEl = document.querySelector('#ingredients');
 // });
 
 
-// Вариант 2
+// Вариант 3
 
-let ingredientName = document.createElement('ul');
+// let ingredientName = document.createElement('ul');
 
-ingredients.forEach(item => {
-    const liEl = document.createElement('li');
-    liEl.textContent = item;
-    ingredientName.appendChild(liEl);
-});
+// ingredients.forEach(item => {
+//     const liEl = document.createElement('li');
+//     liEl.textContent = item;
+//     ingredientName.appendChild(liEl);
+// });
 
-ulEl.innerHTML = ingredientName.innerHTML;
+// ulEl.innerHTML = ingredientName.innerHTML;
