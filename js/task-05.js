82096ba9b05f9aa50badef81f7afeec085de93db
Serj-New <njersdesign@gmail.com>
2021-04-08 14:31:57 +0300

    исправляет задачу 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,17 @@
 const nameIn = document.querySelector('#name-input');
 const nameOut = document.querySelector('#name-output');
 
+// Вариант 1 (тернарник)
 nameIn.addEventListener("input", event => {
-    if (nameIn.value.length !== 0) {
-        return nameOut.textContent = event.currentTarget.value
-    }
-    return nameOut.textContent = `незнакомец`
-})
+    nameIn.value.length !== 0 ? (nameOut.textContent = event.currentTarget.value) : (nameOut.textContent = 'незнакомец')
+});
+
+// Вариант 2 (if else)
+// nameIn.addEventListener("input", event => {
+//     let nameLabel = 'незнакомец';
+
+//     if (nameIn.value.length !== 0) {
+//         nameLabel = event.currentTarget.value
+//     }
+//     nameOut.textContent = nameLabel
+// });
